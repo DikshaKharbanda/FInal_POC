3b3a62542eca7bd8a0bba44c3cd6f66559a09d54
DikshaKharbanda <120650837+DikshaKharbanda@users.noreply.github.com>
2025-02-26 19:22:39 +0530

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Initialization file for the utils module.
+"""
+
+from .email_sender import send_report_email  # Corrected import
+from .docx_formatter import populate_template
+
+__all__ = ['send_report_email', 'populate_template']
+
